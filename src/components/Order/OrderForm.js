c7e9da1ed7a4407444fedae9918dd674cf073b1a
bibhutighimire <ghimirebibhuti@gmail.com>
2021-06-23 11:47:25 -0600

    order and layout component added. material ui installed and being used in formjs

---
+++ src/components/Order/OrderForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function OrderForm() {
+    return (
+        <div>
+            <form noValidate autoComplete="off">
+            <input />
+            <select />
+            </form>
+        </div>
+    )
+}
---
+++ src/layouts/Form.js
@@ -0,0 +1,20 @@
+import { makeStyles } from '@material-ui/core'
+import React from 'react'
+
+const useStyles = makeStyles(theme => ( {
+root: {
+    '& .MuiFormControl-root': {
+        width:'90%',
+        margin: theme.spacing(1)
+    }
+}
+})
+)
+
+export default function Form(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
